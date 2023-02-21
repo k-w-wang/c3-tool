617ce27442f5707cc38a4f11c3bcd52ee3ee6d57
k-w-wang <wk980221@icloud.com>
2023-02-21 11:05:58 +0800

    优化获取dom节点

--- src/showModal.tsx
+++ src/showModal.tsx
@@ -28,80 +28,142 @@ const showModal: ({ nodes, edges }: { nodes: any; edges: any }) => void = ({
 		// 初始化canvas
 		useEffect(() => {
 			if (resultConfig.visible) {
-				setTimeout(() => {
-					// const nodes: Array<{ id: string; label: string }> = [];
-					// const edges: Array<{
-					// 	source: string;
-					// 	target: string;
-					// 	label: string;
-					// }> = [];
-					// PathTreePairs.forEach((item) => {
-					// 	nodes.push({
-					// 		id: String(item.root_hopid),
-					// 		label: String(item.root_hopid),
-					// 	});
-					// 	item.child_hopids.forEach((childItem, index) => {
-					// 		edges.push({
-					// 			source: String(item.root_hopid),
-					// 			target: String(childItem),
-					// 			label: String(item.rankings[index]),
-					// 		});
-					// 	});
-					// });
-
+				const domObserver = new MutationObserver((_mutationList, observer) => {
 					const container = document.getElementById("result") as HTMLElement;
-					const width = container.scrollWidth;
-					const height =
-						container.scrollHeight !== 0 ? container.scrollHeight : 500;
-					const layout = {
-						type: "dagre",
-						rankdir: "LR", // 可选，默认为图的中心
-					};
-					const graph = new G6.Graph({
-						container: "result",
-						width,
-						height,
-						layout,
-						// 边的样式
-						defaultEdge: {
-							style: {
-								stroke: "#F6BD16",
-								lineWidth: 2,
-								endArrow: true,
+
+					if (container != null) {
+						observer.disconnect();
+
+						const width = container.scrollWidth;
+						const height =
+							container.scrollHeight !== 0 ? container.scrollHeight : 500;
+						const layout = {
+							type: "dagre",
+							rankdir: "LR", // 可选，默认为图的中心
+						};
+						const graph = new G6.Graph({
+							container: "result",
+							width,
+							height,
+							layout,
+							// 边的样式
+							defaultEdge: {
+								style: {
+									stroke: "#F6BD16",
+									lineWidth: 2,
+									endArrow: true,
+								},
 							},
-						},
-						// 默认节点样式
-						defaultNode: {
-							type: "circle",
-							size: 30,
-							// label样式
-							// labelCfg: {
-							// 	// position: "",
-							// 	offset: 5,
-							// 	style: {
-							// 		fill: "#eee",
-							// 	},
-							// },
-						},
-
-						// linkCenter: true,
-
-						// The sets of behavior modes
-
-						// The node styles in different states
-						nodeStateStyles: {
-							// The node styles in selected state
-							selected: {
-								stroke: "#666",
-								lineWidth: 2,
-								fill: "steelblue",
+							// 默认节点样式
+							defaultNode: {
+								type: "circle",
+								size: 30,
+								// label样式
+								// labelCfg: {
+								// 	// position: "",
+								// 	offset: 5,
+								// 	style: {
+								// 		fill: "#eee",
+								// 	},
+								// },
 							},
-						},
-					});
 
-					graph.data({ nodes, edges });
-					graph.render();
-				}, 20);
+							// linkCenter: true,
+
+							// The sets of behavior modes
+
+							// The node styles in different states
+							nodeStateStyles: {
+								// The node styles in selected state
+								selected: {
+									stroke: "#666",
+									lineWidth: 2,
+									fill: "steelblue",
+								},
+							},
+						});
+
+						graph.data({ nodes, edges });
+						graph.render();
+					}
+				});
+
+				domObserver.observe(document.body, { childList: true, subtree: true });
+
+				// setTimeout(() => {
+				// 	// const nodes: Array<{ id: string; label: string }> = [];
+				// 	// const edges: Array<{
+				// 	// 	source: string;
+				// 	// 	target: string;
+				// 	// 	label: string;
+				// 	// }> = [];
+				// 	// PathTreePairs.forEach((item) => {
+				// 	// 	nodes.push({
+				// 	// 		id: String(item.root_hopid),
+				// 	// 		label: String(item.root_hopid),
+				// 	// 	});
+				// 	// 	item.child_hopids.forEach((childItem, index) => {
+				// 	// 		edges.push({
+				// 	// 			source: String(item.root_hopid),
+				// 	// 			target: String(childItem),
+				// 	// 			label: String(item.rankings[index]),
+				// 	// 		});
+				// 	// 	});
+				// 	// });
+
+				// 	const container = document.getElementById("result") as HTMLElement;
+				// 	const width = container.scrollWidth;
+				// 	const height =
+				// 		container.scrollHeight !== 0 ? container.scrollHeight : 500;
+				// 	const layout = {
+				// 		type: "dagre",
+				// 		rankdir: "LR", // 可选，默认为图的中心
+				// 	};
+				// 	const graph = new G6.Graph({
+				// 		container: "result",
+				// 		width,
+				// 		height,
+				// 		layout,
+				// 		// 边的样式
+				// 		defaultEdge: {
+				// 			style: {
+				// 				stroke: "#F6BD16",
+				// 				lineWidth: 2,
+				// 				endArrow: true,
+				// 			},
+				// 		},
+				// 		// 默认节点样式
+				// 		defaultNode: {
+				// 			type: "circle",
+				// 			size: 30,
+				// 			// label样式
+				// 			// labelCfg: {
+				// 			// 	// position: "",
+				// 			// 	offset: 5,
+				// 			// 	style: {
+				// 			// 		fill: "#eee",
+				// 			// 	},
+				// 			// },
+				// 		},
+
+				// 		// linkCenter: true,
+
+				// 		// The sets of behavior modes
+
+				// 		// The node styles in different states
+				// 		nodeStateStyles: {
+				// 			// The node styles in selected state
+				// 			selected: {
+				// 				stroke: "#666",
+				// 				lineWidth: 2,
+				// 				fill: "steelblue",
+				// 			},
+				// 		},
+				// 	});
+
+				// 	graph.data({ nodes, edges });
+				// 	graph.render();
+				// }, 20);
 			}
 		}, [resultConfig.visible]);
 
